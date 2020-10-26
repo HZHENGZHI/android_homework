8e3e32bf51d428dd39049138f4aa379b0454670e
HZHENGZHI <1194365836@qq.coom>
2020-10-26 08:34:40 +0800

    e

---
+++ 电子商务/cookie/index.js
@@ -0,0 +1,10 @@
+$(".login").click(function (e) { 
+    var id=$(".id").val();
+    var pw=$(".pw").val();
+
+  {  $.cookie('id',id,{expires:7});
+    $.cookie("pw",pw,{expires:7});}
+
+    console.log($.cookie("id"))
+    console.log($.cookie('pw'))
+});
