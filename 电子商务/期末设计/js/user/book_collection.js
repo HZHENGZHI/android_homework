1eba8af2b16ac5ece0180161f8dc1abfe4cd8f8a
HZHENGZHI <1194365836@qq.coom>
2020-12-13 18:06:48 +0800

    22

--- 电子商务/期末设计/js/user/book_collection.js
+++ 电子商务/期末设计/js/user/book_collection.js
@@ -1,17 +1,37 @@
 
 var path=[];
 path[0]="https://img2.doubanio.com/view/ark_article_cover/retina/public/24283113.jpg?v=0"
-path[1]="https://img1.doubanio.com/view/ark_article_cover/retina/public/28795069.jpg?v=0"
+
 var temp=0
 
+
+const TYPES = ['info', 'warning', 'success', 'error'],
+TITLES = {
+    'info': '注意！',
+    'success': '成功！',
+    'warning': '库存不足!',
+    'error': '出现未知错误!'
+},
+CONTENT = {
+    'info': 'Hello, world! This is a toast message.',
+    'success': '加入购物车成功!',
+    'warning': '加入购物车失败!',
+    'error': '库存不足!'
+},
+POSITION = ['top-right', 'top-left', 'top-center', 'bottom-right', 'bottom-left', 'bottom-center'];
+
+$.toastDefaults.position = 'bottom-right';
+$.toastDefaults.dismissible = true;
+$.toastDefaults.stackable = true;
+$.toastDefaults.pauseDelayOnHover = true;
+
+
+
 $('#table').bootstrapTable({
+    search:true,
+    uniqueId:'IBSN',
     columns: [
-    // {
-    //     field: 'id',
-    //     title: '序号',
-    //     checkbox:true,
-    
-    // },
+   
     {
         field:'picture',
         title:'书本封面',
@@ -20,41 +40,86 @@ $('#table').bootstrapTable({
             item.id=false;
             if(temp<path.length)
             {
-                var btnfix="<img src="+path[temp]+" class='img-fluid' alt='Responsive image'style='height: 75px; width: 50px;'>"
+                var img="<img src="+path[temp]+" class='img-fluid' alt='Responsive image'style='height: 75px; width: 50px;'>"
                 temp++;
             }
            
-            return btnfix
+            return img
         },
         width:80
     },
     {
         field:'name',
         title:'书籍名称',
+        width:400,
+        cellStyle:{
+            css:
+            {
+               " text-align":"center",
+               "font-size": "x-large"
+            }
+        }
     },
     {
         field:'IBSN',
-        title:'I12N',
+        title:'IBSN',
+        width:400,
+        cellStyle:{
+            css:
+            {
+               "text-align":"center",
+               "font-size": "x-large"
+            }
+        }
     },
     {
         field:'price',
-        title:'价格'
+        title:'价格',
+        cellStyle:{
+            css:
+            {
+               " text-align":"center",
+               "font-size": "x-large"
+            }
+        }
     },
     {
         field:'opertion',
         title:'操作',
-        // events:
-        // {
-        //    'click .back':function(e,value,row,index)
-        //    {
-        //     //    console.log(row);
-        //    }
-        // },
+        events:
+        {
+           'click .delmove':function(e,value,row,index)
+           {
+            alert("是否移出收藏夹")
+            console.log(temp);
+            $('#table').bootstrapTable('removeByUniqueId',row.IBSN)
+            temp=index+1;
+            console.log(temp);
+           },
+           'click .bttn':function(e,value,row,index)
+           {
+            var rng = 1,
+            type = TYPES[2],
+            title = TITLES[type],
+            content = CONTENT[type];
+
+            if (rng === 1) {
+            $.toast({
+                type: type,
+                title: title,
+                subtitle: '',
+                content: content,
+                delay: 5000
+            });
+        }
+           }
+
+        },
         formatter:function(value,item,index)
             {
                 item.id=false;
-                var btnfix=" <button type='button' class='btn btn-primary back' style='margin-right: 15px;'>加入购物车</button>"
-                +"<button type='button' class='btn btn-primary back' style='margin-right: 15px;'>加入收藏</button>"
+                var btnfix=" <button type='button' class='btn btn-primary bttn' style='margin-right: 15px;'>加入购物车</button>"
+                +"<button type='button' class='btn btn-primary delmove' style='margin-right: 15px;'>移出收藏</button>"
                 return btnfix
             },
             width:500,
@@ -63,10 +128,11 @@ $('#table').bootstrapTable({
     data:
     [
         {
-        },
-        {
-
-        },
+            
+            name:'哈姆雷特',
+            IBSN:'9787552701074',
+            price:'123'
+        }
     ]
     
 })
