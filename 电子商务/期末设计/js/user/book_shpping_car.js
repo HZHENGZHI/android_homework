ed68d724545e28fd06d7927f0fa8cf158ed2a5e6
HZHENGZHI <1194365836@qq.coom>
2020-12-15 14:32:58 +0800

    2:

---
+++ 电子商务/期末设计/js/user/book_shpping_car.js
@@ -0,0 +1,17 @@
+$('#table').bootstrapTable({
+    // search:true,
+    // pagination:true,
+    // paginationLoop:true,
+    // toolbar:'toolbar',
+    // local:'zh-CN',
+    // url:"",
+    // method:"GET",
+    // toolbar:'#toolbar',//工具栏
+   
+columns: [{
+    field: 'id',
+    title: '序号',
+    checkbox:true,
+
+}]
+})
